b7cd215b7350229ed328194d6d1c8ada116e3856
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2023-02-03 19:15:49 +1100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'Jnet', // 作者
   BIO: 'Chinese Section', // 作者简介
-  LINK: 'https://jnet-chinese.vercel.app/', // 网站地址
+  LINK: 'https://jnetchinese.vercel.app/', // 网站地址
   KEYWORDS: '', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID: '162b05d9c4384d3c8f245c1c207c0b18', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
